185d1fd4fadf4e555d57577dd91b13f5f8c08072
gpjen95 <nyonggpj9995@gmail.com>
2023-10-24 20:21:43 +0900

    add: logic

--- go/main.go
+++ go/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Patient struct {
+	MRnumber string
+	Gender   string
+}
+
+type HospitalQueue struct {
+	Queue []Patient
+}
+
+func (h *HospitalQueue) HandleIN() {
+	fmt.Println("handle in")
+}
 
 func main() {
-	fmt.Println("oke")
+	fmt.Println(" ===[ Hospital Queue Aplication ]===")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		command := strings.Fields(input)
+
+		if len(command) == 0 {
+			continue
+		}
+
+		switch strings.ToUpper(command[0]) {
+		case "IN":
+			fmt.Println("case in")
+		case "OUT":
+			fmt.Println("case OUT")
+		case "ROUNDROBIN":
+			fmt.Println("case ROUNDROBIN")
+		case "DEFAULT":
+			fmt.Println("case DEFAULT")
+		case "EXIT":
+			fmt.Println("case EXIT")
+			return
+		default:
+			fmt.Println("case default")
+		}
+	}
+
 }
